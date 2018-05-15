86e9c41bf1c9c6ff39a220a4c1120b05000e9c9d
M4xW4n9 <aboultraman@gmail.com>
2018-05-15 10:18:40 +0800

    ppc

--- inndy_fast/exp.py
+++ inndy_fast/exp.py
@@ -1,50 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__Auther__ = "M4x"
+__Auther__ = 'M4x'
 
 from pwn import *
-#  from subprocess import Popen, PIPE
-from math import ceil
 from numpy import int32
-import sys
 import time
 import re
 #  context.log_level = 'debug'
 
-if sys.argv[1] == "l":
-    io = process("./fast")
-else:
-    io = remote("hackme.inndy.tw", 7707)
+#  io = process("./fast")
+io = remote("hackme.inndy.tw", 7707)
 
 io.sendlineafter("the game.\n", "Yes I know")
 
-cnt = 0
-last = '\n'
+ans = ""
+res = ""
+f = lambda x: int32(int(x))
+for i in xrange(10000):
+    n1, op, n2 = io.recvuntil("=", drop = True).strip().split(' ')
+    #  print n1, op, n2
+    io.recvline()
 
-while cnt <= 10000:
-    #  time.sleep(0.01)
-    success("%d exps calculated!" % cnt)
-    try:
-        text = last.strip() + io.recv(1000)
-    except:
-        text = last.strip() + io.recv()
-    #  print text
-    exps = re.split(r"\s?=\s?\?\n", text)
-    #  print exps
+    if op == '+':
+        #  print n1, op, n2
+        ans = str(f(n1) + f(n2))
+    if op == '-':
+        ans = str(f(n1) - f(n2))
+    if op == '*':
+        ans = str(f(n1) * f(n2))
+    if op == '/':
+        ans = str(int(float(n1) / int(n2)))
 
-    last = exps[-1]
-    exps = exps[: -1]
+    res += (ans + " ")
 
-    cnt += len(exps)
-    #  result = [str(ctypes.c_int32(int(ceil(eval(i)))).value) + "\n" for i in exps]
-    #  io.send(''.join(result))
-
-    
-    for exp in exps:
-        #  exp = exp.split()
-        n1, op, n2 = exp.split()
-        exp = str(int32(n1)) + op + str(int32(n2))
-        io.send(str(int32(ceil(eval(exp)))))
-
+#  print res
+io.sendline(res)
 io.interactive()
 io.close()
+
+
