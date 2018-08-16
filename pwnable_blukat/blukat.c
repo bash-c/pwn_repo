69808271a5925ad90689569aafe2c5824281dc56
0x01f <aboultraman@gmail.com>
2018-08-16 19:11:33 +0800

    linux group perm

---
+++ pwnable_blukat/blukat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+char flag[100];
+char password[100];
+char* key = "3\rG[S/%\x1c\x1d#0?\rIS\x0f\x1c\x1d\x18;,4\x1b\x00\x1bp;5\x0b\x1b\x08\x45+";
+void calc_flag(char* s){
+	int i;
+	for(i=0; i<strlen(s); i++){
+		flag[i] = s[i] ^ key[i];
+	}
+	printf("%s\n", flag);
+}
+int main(){
+	FILE* fp = fopen("./password", "r");
+	fgets(password, 100, fp);
+	char buf[100];
+	printf("guess the password!\n");
+	fgets(buf, 128, stdin);
+	if(!strcmp(password, buf)){
+		printf("congrats! here is your flag: ");
+		calc_flag(password);
+	}
+	else{
+		printf("wrong guess!\n");
+		exit(0);
+	}
+	return 0;
+}
+
