2ab7a6ea51bdb6d907f3c14c12ffc6e3ab90d0e7
bash-c <aboultraman@gmail.com>
2018-09-30 10:50:08 +0800

    kernel UAF

---
+++ CISCN2017_babydriver/boot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+qemu-system-x86_64 -initrd rootfs.cpio -kernel bzImage -append 'console=ttyS0 root=/dev/ram oops=panic panic=1' -enable-kvm -monitor /dev/null -m 64M --nographic  -smp cores=1,threads=1 -cpu kvm64,+smep
