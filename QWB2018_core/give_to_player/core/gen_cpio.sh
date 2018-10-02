b8491ebc91402fca8b7e53f354f65403c6636988
bash-c <aboultraman@gmail.com>
2018-10-02 11:53:33 +0800

    kernel ROP

--- QWB2018_core/give_to_player/core/gen_cpio.sh
+++
@@ -1,3 +0,0 @@
-find . -print0 \
-| cpio --null -ov --format=newc \
-| gzip -9 > $1
--- QWB2018_core/give_to_player/start.sh
+++
@@ -1,8 +0,0 @@
-qemu-system-x86_64 \
--m 128M \
--kernel ./bzImage \
--initrd  ./core.cpio \
--append "root=/dev/ram rw console=ttyS0 oops=panic panic=1 quiet kaslr" \
--s  \
--netdev user,id=t0, -device e1000,netdev=t0,id=nic0 \
--nographic  \
