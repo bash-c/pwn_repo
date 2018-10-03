2ae8f2c77a640e781796efef738ecc5b27b9e3dd
bash-c <aboultraman@gmail.com>
2018-10-03 18:05:37 +0800

    kernel syscall

---
+++ pwnable_syscall/syscall.c
@@ -0,0 +1,45 @@
+// adding a new system call : sys_upper
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/vmalloc.h>
+#include <linux/mm.h>
+#include <asm/unistd.h>
+#include <asm/page.h>
+#include <linux/syscalls.h>
+
+#define SYS_CALL_TABLE		0x8000e348		// manually configure this address!!
+#define NR_SYS_UNUSED		223
+
+//Pointers to re-mapped writable pages
+unsigned int** sct;
+
+asmlinkage long sys_upper(char *in, char* out){
+	int len = strlen(in);
+	int i;
+	for(i=0; i<len; i++){
+		if(in[i]>=0x61 && in[i]<=0x7a){
+			out[i] = in[i] - 0x20;
+		}
+		else{
+			out[i] = in[i];
+		}
+	}
+	return 0;
+}
+
+static int __init initmodule(void ){
+	sct = (unsigned int**)SYS_CALL_TABLE;
+	sct[NR_SYS_UNUSED] = sys_upper;
+	printk("sys_upper(number : 223) is added\n");
+	return 0;
+}
+
+static void __exit exitmodule(void ){
+	return;
+}
+
+module_init( initmodule );
+module_exit( exitmodule );
+
